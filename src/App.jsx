e249cca13b4d4725bf5f1b6736a2d70148d82fb3
habit <habti2656@gmail.com>
2024-01-30 17:37:22 +0300

    routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signup from './Pages/Signup'
+import Signin from './Pages/Signin'
+import Projects from './Pages/Projects'
+import Dashboard from './Pages/Dashboard'
 
 export default function mern() {
   return (
-    <div>mern</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/about' element = {<About/>}/>
+      <Route path='/sign-up' element = {<Signup/>}/>
+      <Route path='/sign-in' element = {<Signin/>}/>
+      <Route path='/projects' element = {<Projects/>}/>
+      <Route path='/dashboard' element = {<Dashboard/>}/>
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
